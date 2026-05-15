5283c2ca9e84f154b2544017e37342e3d97f0fb1
Deepak GM <deeeepakgm@gmail.com>
2026-05-15 08:08:02 +0530

    Disabled MongoDB for Render deployment

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,11 +1,3 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+events_collection = []
 
-MONGO_URL = "mongodb://localhost:27017"
-
-client = AsyncIOMotorClient(MONGO_URL)
-
-database = client.disasterguard
-
-events_collection = database.events
-
-alerts_collection = database.alerts
+alerts_collection = []
